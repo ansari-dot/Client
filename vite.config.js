9f14632da6f0dcec8e01e3a721852aa161413ebd
ansari-dot <0349ansari@gmail.com>
2025-07-24 23:12:41 +0500

    update changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set the base URL for the application
+  server: {
+    historyApiFallback: true,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 })
